a63e2a447d0017a0a30f6799c74b6d36b497a562
alex <alexbonavila44@gmail.com>
2017-06-04 18:46:52 +0200

    Bugfix

--- app/Http/Controllers/GcmTokensController.php
+++ app/Http/Controllers/GcmTokensController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\GcmToken;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 class GcmTokensController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     /**
      * Add gcm token to user.
      * @param Request $request
@@ -17,6 +23,7 @@ class GcmTokensController extends Controller
         $user->gcmTokens()->firstOrCreate([
             'registration_id' => $request->input('registration_id')
         ]);
+
         return ['status' => 'Token saved!'];
     }
 }
